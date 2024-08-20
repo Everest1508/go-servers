6c189845343f28bdcd5ac25c599dc428cef2d352
Everest1508 <riteshmahale15@gmail.com>
2024-08-20 12:31:59 +0530

    CLIENT: Read and Write functionality to client, SERVER: Client Logs

--- tcp/handle.go
+++ tcp/handle.go
@@ -5,13 +5,19 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 func Handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Println("~~~connection timeout~~~")
+	}
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		ln := scanner.Text()
 		fmt.Println(ln)
+		fmt.Fprintf(conn, ">>%s\n", ln)
 	}
 
 	defer func() {
